a90271d2b799938de133e7205edfc285005a16b7
Chris Keller <xylo04@gmail.com>
2023-02-23 22:28:46 -0700

    Use an accordion to clean up login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
@@ -34,9 +35,9 @@ import { UserSettingsComponent } from './user-settings/user-settings.component';
     AvatarComponent,
     HomeComponent,
     LoginComponent,
+    ScheduleCellComponent,
     ScheduleComponent,
     UserSettingsComponent,
-    ScheduleCellComponent,
   ],
   imports: [
     AngularFireAuthModule,
@@ -50,16 +51,17 @@ import { UserSettingsComponent } from './user-settings/user-settings.component';
     MatButtonModule,
     MatCardModule,
     MatDialogModule,
+    MatExpansionModule,
     MatIconModule,
     MatInputModule,
     MatListModule,
     MatMenuModule,
+    MatSelectModule,
     MatSidenavModule,
     MatSnackBarModule,
     MatTableModule,
     MatToolbarModule,
     ReactiveFormsModule,
-    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
