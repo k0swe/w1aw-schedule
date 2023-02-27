8cfd2fce448612b0d2f78a2cadae9b81c1f0357a
Chris Keller <xylo04@gmail.com>
2023-02-26 21:33:52 -0700

    Change site title (#34)
    Fixes #29

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  appName: 'W1AW/0 Schedule',
+  appName: 'W1AW/0 Colorado Schedule',
   production: true,
   firebase: {
     apiKey: 'AIzaSyDSbBxH0ackVF6QM-Y6EG7Vsj9xQ8VczJA',
