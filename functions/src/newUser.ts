0c5f0c53d29af2aa0d24862f81400b65c7da0b3b
Chris Keller <xylo04@gmail.com>
2023-03-06 22:11:04 -0700

    Add welcome emails

--- functions/src/newUser.ts
+++ functions/src/newUser.ts
@@ -7,4 +7,24 @@ export const newUser = functions.auth.user().onCreate(async (user) => {
     .collection('users')
     .doc(user.uid)
     .set({ email: user.email, status: 'Provisional', name: user.displayName });
+
+  await admin
+    .firestore()
+    .collection('mail')
+    .doc()
+    .set({
+      to: user.email,
+      bccUids: [
+        'r9qBLFDsymTyrWb3vtJmhZlDPMy1',
+        'r3crDouTiXWqBSvRB9aS1afKgPU2',
+        'wgIC96oiqIh65tpT2U9aplvQMpo2',
+        'Q7KNTXh5zTZBxJoRiqPyT9oWMhn2',
+      ],
+      template: {
+        name: 'welcome',
+        data: {
+          email: user.email,
+        },
+      },
+    });
 });
