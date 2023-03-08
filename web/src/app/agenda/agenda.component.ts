9db0ac25c03cce50f4c0f39503d866b4ad8ace0c
Chris Keller <xylo04@gmail.com>
2023-03-08 11:23:23 -0700

    Add links to ICS feed/download
    Fixes #10

--- web/src/app/agenda/agenda.component.ts
+++ web/src/app/agenda/agenda.component.ts
@@ -1,6 +1,9 @@
+import { Clipboard } from '@angular/cdk/clipboard';
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { BehaviorSubject } from 'rxjs';
 
+import { environment } from '../../environment/environment';
 import { AuthenticationService } from '../authentication/authentication.service';
 import { ScheduleService } from '../schedule/schedule.service';
 import { Shift } from '../schedule/shared-constants';
@@ -12,20 +15,35 @@ import { Shift } from '../schedule/shared-constants';
 })
 export class AgendaComponent {
   userShifts$ = new BehaviorSubject<Shift[]>([]);
+  icsLink = '';
 
   constructor(
     private authenticationService: AuthenticationService,
-    private scheduleService: ScheduleService
+    private clipboard: Clipboard,
+    private scheduleService: ScheduleService,
+    private snackBarService: MatSnackBar
   ) {
     this.authenticationService.user$.subscribe((user) => {
       if (!user) {
         return;
       }
+      this.icsLink = `${environment.functionBase}/calendar?uid=${user.uid}`;
       this.scheduleService.findUserShifts(user.uid).subscribe((shifts) => {
         // sort by timestamp, ascending
         shifts.sort((a, b) => a.time.toMillis() - b.time.toMillis());
         this.userShifts$.next(shifts);
       });
     });
+  }
+
+  copyIcsLink() {
+    this.clipboard.copy(this.icsLink);
+    this.snackBarService.open('Copied to clipboard', undefined, {
+      duration: 2000,
+    });
+  }
+
+  downloadIcs() {
+    window.open(this.icsLink, '_blank');
   }
 }
--- web/src/environment/environment.ts
+++ web/src/environment/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   appName: 'W1AW/0 Colorado Schedule',
+  functionBase: `https://us-central1-w1aw-schedule.cloudfunctions.net`,
   production: false,
   firebase: {
     apiKey: 'AIzaSyDSbBxH0ackVF6QM-Y6EG7Vsj9xQ8VczJA',
