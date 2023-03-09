46fe21489a9c5e8db7a663f213a210c16a4de3a7
Chris Keller <xylo04@gmail.com>
2023-03-08 17:33:36 -0700

    Generate icalendar with all reserved shifts

--- functions/src/calendar.ts
+++ functions/src/calendar.ts
@@ -6,41 +6,47 @@ import { COLORADO_DOC_ID } from './shared-constants';
 import * as getUuid from 'uuid-by-string';
 
 export const calendar = functions.https.onRequest(async (request, response) => {
+  let title = 'W1AW/0 Colorado schedule';
+
   const uid = request.query.uid?.toString();
-  if (!uid) {
-    response.status(httpConstants.HTTP_STATUS_BAD_REQUEST);
-    response.send('No uid provided');
-    return;
+  if (uid) {
+    const stationSnapshot = await admin
+      .firestore()
+      .collection('users')
+      .doc(uid)
+      .get();
+    if (!stationSnapshot.exists) {
+      response.status(httpConstants.HTTP_STATUS_BAD_REQUEST);
+      response.send('No station found for uid');
+      return;
+    }
+    title += ` for ${stationSnapshot.data()?.callsign}`;
   }
 
-  const stationSnapshot = await admin
-    .firestore()
-    .collection('users')
-    .doc(uid)
-    .get();
-  if (!stationSnapshot.exists) {
-    response.status(httpConstants.HTTP_STATUS_BAD_REQUEST);
-    response.send('No station found for uid');
-    return;
-  }
-
-  const shiftsQuerySnapshot = await admin
+  const shiftsCollection = admin
     .firestore()
     .collection('sections')
     .doc(COLORADO_DOC_ID)
-    .collection('shifts')
-    .where('reservedBy', '==', uid)
-    .get();
+    .collection('shifts');
+  let query: admin.firestore.Query<admin.firestore.DocumentData>;
+  if (uid) {
+    query = shiftsCollection.where('reservedBy', '==', uid);
+  } else {
+    query = shiftsCollection.where('reservedBy', '!=', null);
+  }
+  const shiftsQuerySnapshot = await query.get();
 
   const calendar = ical({
-    name: `W1AW/0 schedule for ${stationSnapshot.data()?.callsign}`,
+    name: title,
   });
   shiftsQuerySnapshot.forEach((doc) => {
     calendar.createEvent({
       id: getUuid(doc.id),
       start: doc.data().time.toDate(),
       end: new Date(doc.data().time.toDate().getTime() + 2 * 60 * 60 * 1000),
-      summary: `Operate W1AW/0 on ${doc.data().band}m ${doc.data().mode}`,
+      summary: `${doc.data().reservedDetails.callsign} operate W1AW/0 on ${
+        doc.data().band
+      }m ${doc.data().mode}`,
     });
   });
   response.setHeader('content-type', 'text/calendar');
